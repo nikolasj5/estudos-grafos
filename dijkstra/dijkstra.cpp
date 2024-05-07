49191677da6430f1cd269117f9792fc8af7b9450
Nikolas Makiya Vichi <nikolasj5@gmail.com>
2024-05-06 22:57:28 -0300

    Working version of Dijkstra

--- dijkstra/dijkstra.cpp
+++ dijkstra/dijkstra.cpp
@@ -13,15 +13,16 @@ int main(int argc, char *argv[]){
     int M, N, nz;   
     int firstVert;
 
+/*
     if (argc == 2) { 
         if ((f = fopen(argv[1], "r")) == NULL) {
             fprintf(stderr, "%s is not a valid mtx filename\n", argv[1]);
             exit(1);
         }
 
-        firstVert = 1;  
-    }
-/*
+        firstVert = 0;  
+    }*/
+
     if (argc < 3 && argc != 2){
 		fprintf(stderr, "Usage: %s [martix-market-filename] [initial vertex]\n", argv[0]);
 		exit(1);
@@ -38,7 +39,7 @@ int main(int argc, char *argv[]){
             fprintf(stderr, "Vertex index starts in 1\n", argv[1]);
             exit(1);
         }
-    }*/
+    }
 
     if (mm_read_banner(f, &matcode) != 0)
     {
@@ -62,62 +63,46 @@ int main(int argc, char *argv[]){
         adjacency[fj][fi] = adjacency[fi][fj];
     }
     fclose(f);
-/*
-    for (int i = 0; i < N; i++){
-        for (int j = 0; j < N; j++){
-            if (adjacency[i][j] < 0) cout << adjacency[i][j] << endl;
-        }
-    }
-*/
 
-    vector<int> previousVert;
-    vector<bool> alreadyVisited;
-    vector<int> key;
+    vector<int> route;
+    vector<int> cost;
     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> heap;
     pair<int,int> auxVert;
 
     // Initializing variables
-    previousVert.resize(M, -1);
-    alreadyVisited.resize(M, false);
-    key.resize(M, INT_MAX);
-
-    /*
-    auxVert.first = INT_MAX;
-    for (int i = 0; i < M; i++){
-        if (i != firstVert){
-            auxVert.second = i;
-            heap.push(auxVert);
-        }
-    }
-    */
+    route.resize(M, -1);
+    cost.resize(M, INT_MAX);
 
     auxVert.first = 0;
     auxVert.second = firstVert;
     heap.push(auxVert);
-    key[firstVert] = 0;
+    route[firstVert] = firstVert;
+    cost[firstVert] = 0;
 
-    int cost, index;
+    int weight, index;
     while (!heap.empty()){
         auxVert = heap.top();
         heap.pop();
-        cost = auxVert.first;
+        weight = auxVert.first;
         index = auxVert.second;
 
-        if (alreadyVisited[index] != true){
-            alreadyVisited[index] = true;
-
-            for (int i = 0; i < M; i++){
-                if (alreadyVisited[i] == false && key[i] > adjacency[index][i]){
-                    key[i] = adjacency[index][i];
-                    heap.push({key[i], i});
-                    previousVert[i] = index;
-                }
+        for (int i = 0; i < M; i++){
+            if (adjacency[index][i] != 0 && cost[i] > adjacency[index][i] + weight){
+                route[i] = index;
+                cost[i] = adjacency[index][i] + weight;
+                heap.push({cost[i], i});
             }
         }
     }
 
+    printf("Costs:\n");
     for (int i = 0; i < M; i++){
-        printf("%d %d\n", i+1, (previousVert[i]==-1)?(-1):previousVert[i]+1);
+        printf("%d %d\n", i+1, cost[i]);
+    }
+
+    printf("\n\nRoute:\n");
+    for (int i = 0; i < M; i++){
+        printf("%d %d\n", i+1, route[i]);
     }
 
     return 0;
